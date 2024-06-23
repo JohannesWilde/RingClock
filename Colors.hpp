4a94d1666bfad3fdd4059ff39a97ac9a5eba0edd
Johannes Wilde <git@jowil.de>
2024-06-23 22:10:58 +0200

    Distinguish between LED strips with 4 and 3 color components.

--- Colors.hpp
+++ Colors.hpp
@@ -9,18 +9,10 @@ namespace Colors
 typedef uint32_t Color_t;
 
 /* copied from Adafruid_NeoPixel - made constexpr as to minimize flash usage */
-constexpr Color_t Color(uint8_t r, uint8_t g, uint8_t b, uint8_t w)
+constexpr Color_t Color(uint8_t r, uint8_t g, uint8_t b, uint8_t w = 0)
 {
     return ((Color_t)w << 24) | ((Color_t)r << 16) | ((Color_t)g <<  8) | (Color_t)b;
 }
-
-Color_t constexpr Black = Color(0, 0, 0, 0);
-Color_t constexpr Red = Color(255, 0, 0, 0);
-Color_t constexpr Green = Color(0, 255, 0, 0);
-Color_t constexpr Blue = Color(0, 0, 255, 0);
-Color_t constexpr Yellow = Color(255, 246, 0, 0);
-Color_t constexpr White = Color(0, 0, 0, 255);
-Color_t constexpr Gray = Color(0, 0, 0, 60);
 
 // Scale components independently to range [0, 255]. Outliers will be clipped.
 Color_t colorScaleBrightness(Color_t const & input, double const & scaleFactor);
@@ -28,6 +20,22 @@ Color_t colorScaleBrightness(Color_t const & input, double const & scaleFactor);
 // Sum up components of colors independently. Saturates at 0xff for each component.
 Color_t addColors(Color_t const & one, Color_t const & two);
 
-}
+Color_t constexpr Black     = Color(0, 0, 0);
+Color_t constexpr Red       = Color(255, 0, 0);
+Color_t constexpr Green     = Color(0, 255, 0);
+Color_t constexpr Blue      = Color(0, 0, 255);
+Color_t constexpr Yellow    = Color(255, 246, 0);
+Color_t constexpr White     = Color(255, 255, 255);
+Color_t constexpr Gray      = Color(60, 60, 60);
+
+namespace WhithWhite
+{
+
+Color_t constexpr White     = Color(0, 0, 0, 255);
+Color_t constexpr Gray      = Color(0, 0, 0, 60);
+
+} // namespace WhithWhite
+
+} // namespace Colors
 
 #endif // COLORS_HPP
